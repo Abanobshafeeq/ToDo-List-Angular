07942d2b14eb480d516938295896d224f56e1e8f
Abanobshafeeq <abanobshafeeq100@gmail.com>
2026-02-09 11:20:43 +0200

    add task-card

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ITask } from '../../models/task/task.module';
 @Component({
   selector: 'app-task-card',
   standalone: true,
   imports: [],
   templateUrl: './task-card.component.html',
-  styleUrl: './task-card.component.css'
+  styleUrl: './task-card.component.css',
 })
 export class TaskCardComponent {
-
+  @Input() task!: ITask;
+  @Output() deleteTask = new EventEmitter<number>();
+  @Output() editTask = new EventEmitter<ITask>();
 }
