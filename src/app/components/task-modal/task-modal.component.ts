efc6f125b7f0505358469c23caa671c369296731
Abanobshafeeq <abanobshafeeq100@gmail.com>
2026-02-09 15:45:12 +0200

    Finish Todo

--- src/app/components/task-modal/task-modal.component.ts
+++ src/app/components/task-modal/task-modal.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ITask } from '../../models/task/task.module';
+declare var bootstrap: any;
 @Component({
   selector: 'app-task-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-modal.component.html',
   styleUrl: './task-modal.component.css'
 })
 export class TaskModalComponent {
+  @Output() saveTask = new EventEmitter<ITask>();
+  task: ITask = {
+    id: 0,
+    title: '',
+    description: ''
+  };
+  isEditMode: boolean = false;
+  private modalInstance : any;
 
+  open(taskToEdit: ITask | null) {
+    const modalElement = document.getElementById('taskModal');
+    if (modalElement) {
+      this.modalInstance = new bootstrap.Modal(modalElement);
+      if (taskToEdit) {
+        this.isEditMode = true;
+        this.task = { ...taskToEdit };
+      } else {
+        this.isEditMode = false;
+        this.task = {
+          id: Date.now(),
+          title: '',
+          description: ''
+        };
+      }
+      this.modalInstance.show();
+    }
+  }
+  onSave() {
+    if (this.task.title.trim()) {
+      this.saveTask.emit(this.task);
+      this.modalInstance.hide();
+    } 
+  }
+
+  close() {
+    this.modalInstance.hide();
+  }
 }
--- src/app/components/todo-board/todo-board.component.ts
+++ src/app/components/todo-board/todo-board.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ViewChild, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import {
   CdkDragDrop,
   moveItemInArray,
@@ -8,50 +8,99 @@ import {
 } from '@angular/cdk/drag-drop';
 import { ITask } from '../../models/task/task.module';
 import { TaskCardComponent } from '../task-card/task-card.component';
+import { TaskModalComponent } from '../task-modal/task-modal.component';
+
 @Component({
   selector: 'app-todo-board',
   standalone: true,
-  imports: [CommonModule, DragDropModule, TaskCardComponent],
+  imports: [
+    CommonModule,
+    DragDropModule,
+    TaskCardComponent,
+    TaskModalComponent,
+  ],
   templateUrl: './todo-board.component.html',
   styleUrl: './todo-board.component.css',
 })
-export class TodoBoardComponent {
+export class TodoBoardComponent implements OnInit {
+  
+  @ViewChild(TaskModalComponent) taskModal!: TaskModalComponent;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
   todo: ITask[] = [
-    {
-      id: 1,
-      title: 'Task 1',
-      description: 'Description for Task 1',
-    },
-    {
-      id: 2,
-      title: 'Task 2',
-      description: 'Description for Task 2',
-    },
+    { id: 3, title: 'Task 1', description: 'Description for Task 1  ' },
+    { id: 4, title: 'Task 2', description: 'Description for Task 2 ' },
   ];
-  inProgress: ITask[] = [
-    {
-      id: 3,
-      title: 'Task 3',
-      description: 'Description for Task 3',
-    },
-    {
-      id: 4,
-      title: 'Task 4',
-      description: 'Description for Task 4',
-    },
-  ];
-  done: ITask[] = [
-    {
-      id: 5,
-      title: 'Task 5',
-      description: 'Description for Task 5',
-    },
-    {
-      id: 6,
-      title: 'Task 6',
-      description: 'Description for Task 6',
-    },
-  ];
+  inProgress: ITask[] = [];
+  done: ITask[] = [];
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadFromLocalStorage();
+    }
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('todo', JSON.stringify(this.todo));
+    localStorage.setItem('inProgress', JSON.stringify(this.inProgress));
+    localStorage.setItem('done', JSON.stringify(this.done));
+  }
+
+  loadFromLocalStorage() {
+    const savedTodo = localStorage.getItem('todo');
+    const savedInProgress = localStorage.getItem('inProgress');
+    const savedDone = localStorage.getItem('done');
+    
+    if (savedTodo) this.todo = JSON.parse(savedTodo);
+    if (savedInProgress) this.inProgress = JSON.parse(savedInProgress);
+    if (savedDone) this.done = JSON.parse(savedDone);
+  }
+
+  getNextId(): number {
+    const allTasks = [...this.todo, ...this.inProgress, ...this.done];
+    if (allTasks.length === 0) return 1;
+    return Math.max(...allTasks.map(t => t.id)) + 1;
+  }
+
+  openModal(task?: ITask) {
+    if (task) {
+      this.taskModal.open(task);
+    } else {
+      const newId = this.getNextId();
+      this.taskModal.open({ id: newId, title: '', description: '' });
+    }
+  }
+
+  handleSaveTask(task: ITask) {
+    const exists = this.isTaskExists(task.id);
+    
+    if (exists) {
+      this.updateTaskInList(this.todo, task);
+      this.updateTaskInList(this.inProgress, task);
+      this.updateTaskInList(this.done, task);
+    } else {
+      this.todo.push(task);
+    }
+    
+    this.saveToLocalStorage();
+  }
+
+  updateTaskInList(list: ITask[], task: ITask) {
+    const index = list.findIndex((t) => t.id === task.id);
+    if (index !== -1) list[index] = task;
+  }
+
+  isTaskExists(id: number): boolean {
+    return [...this.todo, ...this.inProgress, ...this.done].some(t => t.id === id);
+  }
+
+  deleteTask(taskId: number) {
+    if (confirm('Are you sure you want to delete this task?')) {
+      this.todo = this.todo.filter((t) => t.id !== taskId);
+      this.inProgress = this.inProgress.filter((t) => t.id !== taskId);
+      this.done = this.done.filter((t) => t.id !== taskId);
+      this.saveToLocalStorage();
+    }
+  }
 
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
@@ -68,23 +117,6 @@ export class TodoBoardComponent {
         event.currentIndex,
       );
     }
-  }
-  deleteTask(taskId: number) {
-    this.todo = this.todo.filter((task) => task.id !== taskId);
-    this.inProgress = this.inProgress.filter((task) => task.id !== taskId);
-    this.done = this.done.filter((task) => task.id !== taskId);
-  }
-
-  editTask(task: ITask) {
-    const newTitle = window.prompt('Edit Task Title:', task.title);
-    
-
-    if (!newTitle) return;
-
-    const updatedTask = { ...task, title: newTitle };
-
-    this.todo = this.todo.map((t) => (t.id === updatedTask.id ? updatedTask : t));
-    this.inProgress = this.inProgress.map((t) => (t.id === updatedTask.id ? updatedTask : t));
-    this.done = this.done.map((t) => (t.id === updatedTask.id ? updatedTask : t));
+    this.saveToLocalStorage();
   }
 }
